059547b151ffa1f6da51f70dce6b8c5227a63fde
Amir Daliri <amir.daliri.github@gmail.com>
2022-07-10 22:01:37 +0300

    Initial Commit

---
+++ SimonsVoss/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimonsVoss
+//
+//  Created by Amir Daliri on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
